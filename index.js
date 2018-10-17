c3ed26b25f6e7bdee0191ed9da85ba72b03ce1b5
Kindle_PC <vibhanshurana10>
2018-10-18 01:04:38 +0530

    updateStatus

--- index.js
+++ index.js
@@ -19,14 +19,19 @@ exports.addMessage = functions.https.onRequest((req, res) => {
   
   // Listens for new messages added to /messages/:pushId/original and creates an
 // uppercase version of the message to /messages/:pushId/uppercase
-exports.makeUppercase = functions.database.ref('/messages/{pushId}/original')
-.onCreate((snapshot, context) => {
+exports.updateStatus = functions.database.ref('/RFID/{pushId}/status')
+.onUpdate((snapshot, context) => {
   // Grab the current value of what was written to the Realtime Database.
   const original = snapshot.val();
+  var status;
+    if(original==0)
+        status=1;
+    else
+        status=0; 
   console.log('Uppercasing', context.params.pushId, original);
-  const uppercase = original.toUpperCase();
+  //const uppercase = original.toUpperCase();
   // You must return a Promise when performing asynchronous tasks inside a Functions such as
   // writing to the Firebase Realtime Database.
   // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
-  return snapshot.ref.parent.child('uppercase').set(uppercase);
+  return snapshot.ref.parent.child('status').set(status);
 });
